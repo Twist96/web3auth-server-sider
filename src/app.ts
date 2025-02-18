e21d900406397fc6659524cfd35eb93cc411e8b2
Tochukwu <martins.twist@gmail.com>
2025-02-18 16:33:45 +0100

    log all requests

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,19 @@ import userRoute from "./routes/user";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+app.use("*", (req, res) => {
+  console.log(`404: ${req.method} ${req.path} not found`);
+  res.status(404).json({ message: `Route ${req.path} not found` });
+});
+
 app.use(express.json());
-app.use("/auth", authRoutes).use("/user", userRoute);
+app.use("/auth", authRoutes);
+app.use("/user", userRoute);
 
 const PORT = process.env.PORT || 3000;
 
