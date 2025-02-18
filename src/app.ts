7859cbf9e406d0f4fc17aee314c6384d224f2c0d
Tochukwu <martins.twist@gmail.com>
2025-02-18 16:47:56 +0100

    .

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,14 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/user", userRoute);
+
 app.use("*", (req, res) => {
   console.log(`404: ${req.method} ${req.path} not found`);
   res.status(404).json({ message: `Route ${req.path} not found` });
 });
-
-app.use(express.json());
-app.use("/auth", authRoutes);
-app.use("/user", userRoute);
 
 const PORT = process.env.PORT || 3000;
 
